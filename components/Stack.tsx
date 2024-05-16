66a33ed57c504d8db1be1213cdc7d7e5fa80408f
njordulv <njordulv@gmail.com>
2024-05-16 22:22:13 +0300

    ADD: tech stack icons to config

--- components/Stack.tsx
+++ components/Stack.tsx
@@ -2,44 +2,10 @@
 
 import clsx from 'clsx'
 import { MotionProps, motion } from 'framer-motion'
+import { Tooltip } from '@nextui-org/tooltip'
 import { twMerge } from 'tailwind-merge'
-import { Button } from '@nextui-org/button'
 import { fontMono } from '@/config/fonts'
-import { TbApi } from 'react-icons/tb'
-import {
-  SiHtml5,
-  SiCss3,
-  SiBootstrap,
-  SiTailwindcss,
-  SiJavascript,
-  SiTypescript,
-  SiReact,
-  SiRedux,
-  SiNextdotjs,
-  SiNextui,
-  SiChakraui,
-  SiMui,
-  SiGit,
-  SiGithub,
-  SiGitlab,
-  SiNpm,
-  SiYarn,
-  SiBabel,
-  SiWebpack,
-  SiDocker,
-  SiKubernetes,
-  SiNodedotjs,
-  SiFirebase,
-  SiPhp,
-  SiSymfony,
-  SiMysql,
-  SiPostgresql,
-  SiWordpress,
-  SiFigma,
-  SiSketch,
-  SiJira,
-  SiTrello,
-} from 'react-icons/si'
+import { siteConfig } from '@/config/site'
 
 export const Stack = () => {
   return (
@@ -96,85 +62,13 @@ const StackBlock = () => (
   <Block className="col-span-12 row-span-2 md:col-span-6 bg-opacity-90 text-color shadow-xl">
     <h1 className={clsx(fontMono.variable)}>Tech Stack</h1>
     <ul className="flex flex-col gap-2 mt-6 text-zinc-400">
-      <li className="flex gap-2">
-        <Button
-          isIconOnly
-          color="danger"
-          radius="full"
-          variant="flat"
-          aria-label="HTML"
-          className="shadow-md text-xl"
-        >
-          <SiHtml5 />
-        </Button>
-        <Button
-          isIconOnly
-          color="primary"
-          radius="full"
-          variant="flat"
-          aria-label="HTML"
-          className="shadow-md text-xl"
-        >
-          <SiCss3 />
-        </Button>
-      </li>
-      <li className="flex gap-2">
-        <SiBootstrap />
-        <SiTailwindcss />
-      </li>
-      <li className="flex gap-2">
-        <SiJavascript />
-        <SiTypescript />
-      </li>
-      <li className="flex gap-2">
-        <SiReact />
-        <SiRedux />
-        <SiNextdotjs />
-      </li>
-      <li className="flex gap-2">
-        <SiNextui />
-        <SiChakraui />
-        <SiMui />
-      </li>
-      <li className="flex gap-2">
-        <SiGit />
-        <SiGithub />
-        <SiGitlab />
-      </li>
-      <li className="flex gap-2">
-        <SiNpm />
-        <SiYarn />
-        <SiBabel />
-        <SiWebpack />
-      </li>
-      <li className="flex gap-2">
-        <SiDocker />
-        <SiKubernetes />
-        <SiNodedotjs />
-      </li>
-      <li className="flex gap-2">
-        <TbApi />
-        <SiFirebase />
-      </li>
-      <li className="flex gap-2">
-        <SiPhp />
-        <SiSymfony />
-      </li>
-      <li className="flex gap-2">
-        <SiMysql />
-        <SiPostgresql />
-      </li>
-      <li className="flex gap-2">
-        <SiWordpress />
-      </li>
-      <li className="flex gap-2">
-        <SiFigma />
-        <SiSketch />
-      </li>
-      <li className="flex gap-2">
-        <SiJira />
-        <SiTrello />
-      </li>
+      {siteConfig.techStackIcons.map((item, index) => (
+        <Tooltip key={index} color="default" content={item.label}>
+          <li key={index} className="flex gap-2">
+            <item.icon size="24" />
+          </li>
+        </Tooltip>
+      ))}
     </ul>
   </Block>
 )
