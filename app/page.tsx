90b9eb629599151c35d23a5545898843a0de0fb8
njordulv <njordulv@gmail.com>
2024-08-07 14:39:02 +0300

    ADD: dynamic imports for main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,42 @@
+import dynamic from 'next/dynamic'
+import { Preloader } from '@/components/Preloader'
 import { Progress } from '@/components/Progress'
 import { About } from '@/components/about'
-import { Stack } from '@/components/stack'
-import { Experience } from '@/components/experience'
-import { Projects } from '@/components/projects'
-import { Beyond } from '@/components/beyond'
-import { Contacts } from '@/components/contacts'
+
+const Stack = dynamic(
+  () => import('@/components/stack').then((mod) => mod.Stack),
+  {
+    loading: () => <Preloader />,
+  }
+)
+
+const Experience = dynamic(
+  () => import('@/components/experience').then((mod) => mod.Experience),
+  {
+    loading: () => <Preloader />,
+  }
+)
+
+const Projects = dynamic(
+  () => import('@/components/projects').then((mod) => mod.Projects),
+  {
+    loading: () => <Preloader />,
+  }
+)
+
+const Beyond = dynamic(
+  () => import('@/components/beyond').then((mod) => mod.Beyond),
+  {
+    loading: () => <Preloader />,
+  }
+)
+
+const Contacts = dynamic(
+  () => import('@/components/contacts').then((mod) => mod.Contacts),
+  {
+    loading: () => <Preloader />,
+  }
+)
 
 export default function Home() {
   return (
