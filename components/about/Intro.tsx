9d43873f1b52700d9fb806ed50b6db51f593158f
njordulv <njordulv@gmail.com>
2024-09-24 18:27:08 +0300

    UPD: into text

--- components/about/Intro.tsx
+++ components/about/Intro.tsx
@@ -14,13 +14,7 @@ export const Intro = () => {
     <Block className="col-span-12 row-span-2 md:col-span-8 flex flex-col bg-none bg-opacity-0 shadow-none text-default border-0 p-0 text-2xl sm:text-3xl">
       <div className={clsx(fontMono.variable)}>
         <h1 className="mb-4">{`Hi, I'm Dmitriy.`}</h1>
-        <p>
-          {`I'm a`}&nbsp;
-          <span className="before:block before:absolute before:-inset-1 before:h-[2px] before:top-[58%] before:bg-zinc-500 relative inline-block before:z-10">
-            <span className="relative text-zinc-500">alcoholic</span>&nbsp;
-          </span>
-        </p>
-        <p>front-end developer.</p>
+        <p>{`I'm a front-end developer.`}</p>
         <p className="text-zinc-400 mb-5">{`Let's connect!`}</p>
       </div>
       <div className="flex gap-4 sm:flex-row flex-col">
