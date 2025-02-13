d802e83bc292c7196df2a1ec0fe85acae63a7878
njordulv <njordulv@gmail.com>
2025-02-13 19:58:32 +0200

    test: file names

--- src/layout.tsx
+++ src/layout.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import { Providers } from '@/Providers'
+import { Providers } from '@/providers'
 import { App } from '@/App'
 import { NotFound } from '@/NotFound'
 import '@/styles/globals.css'
